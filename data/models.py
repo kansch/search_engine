f9c5f562fef092347053546f76414f44a6da979f
kansch <sylvain.kanschine@gmail.com>
2019-09-22 12:13:33 +0200

    Add Calendar model and initial data

--- data/models.py
+++ data/models.py
@@ -5,3 +5,11 @@ class Camper(models.Model):
     location = models.PointField()
     price_per_day = models.FloatField()
     weekly_discount = models.FloatField(default=0)
+
+
+class Calendar(models.Model):
+    """Contains availabilitites for each Camper."""
+    camper = models.ForeignKey(Camper, related_name='calendars', on_delete=models.CASCADE)
+    camper_is_available = models.BooleanField()
+    start_date = models.DateField()
+    end_date = models.DateField()
